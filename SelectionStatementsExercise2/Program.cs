60efc6bf29fe4135eb63ab757fb2e2ccc278fc1c
Josh Daniels <jdaans95@gmail.com>
2024-08-05 11:38:52 -0700

    Selection statement exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+
+            var subject = Console.ReadLine();
+            
+             
+
+            switch(subject)
+            {
+                case "Math":
+                    Console.WriteLine("Math is fun!");
+                break;
+
+                case "English":
+                    Console.WriteLine("English is boring!");
+                break;
+
+                case "Science":
+                    Console.WriteLine("Science is useful");
+                break;
+
+                case "Astronomy":
+                    Console.WriteLine("astronomy is very exciting");
+                break;
+
+                case "World History":
+                    Console.WriteLine("World History can be exciting to learn");
+                break;
+
+
+
+
+
+            }
         }
     }
 }
